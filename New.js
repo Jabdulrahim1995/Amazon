3255ccdbbee15de72dd1acf83da041cd295524a5
Jabdulrahim1995 <j.abdulrahim1995@gmail.com>
2026-09-23 15:51:31 +0530

    First commit

---
+++ New.js
@@ -0,0 +1,167 @@
+
+// // console.log("hello");
+// // console.log(1234);
+// // console.log("abdul \nrahim"); // next line 
+// // console.log("Hello \tword"); // tab
+// //backtick print the exact line
+// // console.log(`hello
+// //     java 
+// //           script`);
+
+// //  console.log(10+5);
+// //  console.log('10' + '20');
+ 
+//  //Variables 
+
+// //  var mh
+
+
+// /*console.log(12365);
+// console.log('Ganesh');
+// console.log("Gopi");
+// */
+// // Single Comments
+
+// /*
+// Multi Line Comments
+
+// I'am "Ganesh"
+// hello
+// bye
+
+// */
+
+// /*
+// console.log('I\'am "Ganesh"');
+// console.log("I'am \"Ganesh\"");
+// console.log('hello \\ bye');
+// console.log('hello \n bye');
+// console.log('hello\tbye');
+// console.log('Hello welcome ');
+// console.log(`hello
+//             Welcome
+//                         to  
+//                                 Javascript
+//                                 I'am "Ganesh"
+// bye
+// `);    
+
+// console.log(10+40);
+// console.log('Ganesh'+'R');
+// console.log('10'+'50');
+// */
+
+// // var PersonName = 'Ganesh';
+// // console.log(PersonName);
+// // var number = 1865;
+// // console.log(number);
+
+// // //redeclare
+// // var PersonName ='Tharani';
+// // console.log(PersonName);
+
+// // //reassign
+// // PersonName = 'Abdhul'
+// // console.log(PersonName);
+
+// // let CM = 'Vijay';
+// // console.log(CM);
+
+// //redeclared  - error occurs
+// // let CM = 'Stalin';
+
+// //reassign
+// // CM = 'Stalin'
+// // console.log(CM);
+
+// // //const
+// // const ACC = 47821478524786;
+// // console.log(ACC);
+// //redeclare - error occurs
+// // const ACC =8548278;
+
+// //reassign - error occurs
+// // ACC = 2527854895;
+// // console.log(ACC);
+
+
+// // let first_name = 'Ganesh' //snake case
+// // let firstName = 'Ganesh'  //camel case
+// // let FirstName = 'Ganesh' //pascal case
+// // let _ =1865;
+// // let for_
+
+// //21th Spe, Variables//
+
+// //Number//
+// let num = 12345;
+// console.log(num);
+// console.log(typeof num);
+
+// let num1 = 1234563443432457435456n;
+// console.log(num1);
+// console.log(typeof num1);
+
+// //string//
+// let name1 = 'abdul'; //single codes//
+// let name2 = "rahim"; //double codes//
+// console.log(name1);
+// console.log(name2);
+// console.log(typeof name1);
+// console.log(typeof name2);
+
+
+// Task1 Basic calculator
+let a = 25;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+//Task2 Employee salary
+let BasicSalary = 25000;
+let Bonus = 5000;
+let deduction = 2000;
+let FinalSalary = BasicSalary + Bonus - deduction;
+console.log(FinalSalary);
+
+//Task3 Comparison operators
+let mark1 = 85;
+let mark2 = 75;
+console.log(mark1 > mark2);
+console.log(mark1 < mark2);
+console.log(mark1 == mark2);
+console.log(mark1 != mark2);
+console.log(mark1 >= mark2);
+console.log(mark1 <= mark2);
+
+//Task4 Function with Arguments 
+let name = 'abdul'
+console.log('hello ' + name);
+
+//Task5 Add three numbers 
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+let sum = num1 + num2 + num3;
+console.log(sum);
+
+//Task6 create a fuction expression to subtract two numbers 
+let numA = 30;
+let numB = 10;
+let subtract = numA - numB;
+console.log(subtract);
+
+//Task7 Addition create an arrow function to add two numbers
+let add = (x, y) => x + y;
+console.log(add(10, 20));
+
+
+
+
+
+
+
